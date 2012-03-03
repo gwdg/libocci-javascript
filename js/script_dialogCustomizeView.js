5d59a7c94f802866a8eda1e7f0b56a5aeaf181a1
athevapalan <andre.thevapalan@tu-dortmund.de>
2012-03-03 20:00:41 +0100

    Customize view added, Layout corrected

---
+++ js/script_dialogCustomizeView.js
@@ -0,0 +1,61 @@
+function createDialogCustomizeView() {
+	$('#button_customize_view').bind('click', function() {
+		$('<div/>', {
+			'id': 'dialog_customize_view',
+			'style' :  'float: left; text-align: left',
+		})
+		.dialog({
+			autoOpen: true,
+			modal: true,
+			title: $.i18n._("Customize_view"),
+			resizable: false,
+			draggable: true,
+			close: function(ev, ui) {
+			    $(this).remove();
+			 }
+		});
+		
+		if($('#dialog_customize_view').dialog("isOpen")) {
+			$('<div/>', {
+				'html' : $.i18n._('Customize_view_text')+":",
+				'style' : 'clear:both',
+			}).appendTo($('#dialog_customize_view'));
+			
+			$.each(KindsOfServer, function(key, value){
+				var term = value.term;
+				var related = value.related;
+				if(related) {
+					var entitySubType = getWordAfterChar(related, '#');
+					if (entitySubType == "resource") {
+						console.log("Adding "+value.term+" to CustomizeView");
+						$('<input/>', {
+							'type' : 'checkbox',
+							'style' :  'text-align: left; margin:5px; float:left',
+							'value' : value.term,
+							'checked' : 'checked'
+						})
+						.bind('change', function(){
+							$('#frame-'+$(this).val()).toggle();
+							if($('#frame-'+$(this).val()).is(':visible')) {
+								$('#main').width($('#main').width() + 330);
+							}else{
+								$('#main').width($('#main').width() - 330);
+							}
+							$('.scroll-pane').jScrollPane();
+						})
+						.after(
+						$('<div/>', {
+							'style' :  'text-align: left; margin:5px; float: left',
+							'html' : value.term
+						}))
+						.appendTo($('#dialog_customize_view'));
+						$('<br/>', {
+							'class' :  'clear',
+						})
+						.appendTo($('#dialog_customize_view'));
+					}
+				}
+			});
+		}
+	});
+}
